cc7e72638df33b57fcf301e40ac71d09fae1c190
Seval Karaosmanoğlu <sevallacin@gmail.com>
2026-03-09 06:56:34 +0000

    last changes

--- models/marts/mart_accepted_loan_loss_summary.sql
+++ models/marts/mart_accepted_loan_loss_summary.sql
@@ -1,16 +1,14 @@
 {{ config(materialized='table') }}
 
 with base as (
-
-    select *
+    select *,
+        PARSE_DATE('%b-%Y', issue_d) as issue_date
     from {{ ref('int_accepted_loan_performance') }}
-
 ),
 
 aggregated as (
-
     select
-        -- segmentation dimensions
+        issue_date,
         grade,
         sub_grade,
         purpose,
@@ -19,31 +17,22 @@ aggregated as (
         credit_limit_segment,
         hardship_segment,
 
-        -- volume
-        count(*)                                   as loan_count,
-        sum(loan_amnt)                             as total_loan_amount,
+        count(*) as loan_count,
+        sum(loan_amnt) as total_loan_amount,
 
-        -- loan status counts
-        sum(is_default)                            as defaulted_loan_count,
-        sum(is_fully_paid)                         as fully_paid_loan_count,
-        sum(is_current)                            as current_loan_count,
+        sum(is_default) as defaulted_loan_count,
+        sum(is_fully_paid) as fully_paid_loan_count,
+        sum(is_current) as current_loan_count,
 
-        -- rates
-        safe_divide(sum(is_default), count(*))     as default_rate,
-
-        -- repayment & recovery
-        sum(total_rec_prncp)                       as total_principal_repaid,
-        sum(recoveries)                            as total_recoveries,
-
-        -- loss
-        sum(loss_amount)                           as total_loss_amount,
-        safe_divide(
-            sum(loss_amount),
-            sum(loan_amnt)
-        )                                          as loss_rate
+        safe_divide(sum(is_default), count(*)) as default_rate,
+        sum(total_rec_prncp) as total_principal_repaid,
+        sum(recoveries) as total_recoveries,
+        sum(loss_amount) as total_loss_amount,
+        safe_divide(sum(loss_amount), sum(loan_amnt)) as loss_rate
 
     from base
     group by
+        issue_date,
         grade,
         sub_grade,
         purpose,
